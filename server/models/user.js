28488320e925182b193248e6226c0a2dbbe7cf6a
mjuferdi <juferdi.mario@yahoo.com>
2018-12-30 22:24:55 +0100

    fix middleware

--- server/models/user.js
+++ server/models/user.js
@@ -1,24 +1,46 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Database schema
 const userSchema = new Schema({
   username: {
     type: String,
-    required: true,
-    max: [50, 'Too long, max is 50 characters']
+    min: [4, 'Too short, min is 4 characters'],
+    max: [32, 'Too long, max is 32 characters']
   },
   email: {
     type: String,
-    required: true,
-    min: [4, 'Too short, min is 4 characters']
+    min: [4, 'Too short, min is 4 characters'],
+    max: [32, 'Too long, max is 32 characters'],
+    unique: true,
+    lowercase: true,
+    required: 'Email is required',
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
   },
   password: {
     type: String,
-    required: true,
-    min: [4, 'Too short, min is 4 characters']
-  }
-
+    min: [4, 'Too short, min is 4 characters'],
+    max: [32, 'Too long, max is 32 characters'],
+    required: 'Password is required'
+  },
+  rentals: [{type: Schema.Types.ObjectId, ref: 'Rental'}]
 });
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.methods.hasSamePassword = function(requestedPassword) {
+
+  return bcrypt.compareSync(requestedPassword, this.password);
+}
+
+
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(user.password, salt, function(err, hash) {
+        user.password = hash;
+        next();
+    });
+  });
+});
+
+module.exports = mongoose.model('User', userSchema );
