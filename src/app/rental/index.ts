07c5f8f51936471085e77eccd7a65f3fe235870f
th30r3tisch <theoretisch@hotmail.de>
2019-01-21 22:51:03 +0100

    refactored rental module imports

---
+++ src/app/rental/index.ts
@@ -0,0 +1,5 @@
+export * from './rental-list/rental-list.component';
+export * from './rental-list-item/rental-list-item.component';
+export * from './rental.component';
+export * from './rental-detail/rental-detail.component';
+export * from './rental-detail/rental-detail-booking/rental-detail-booking.component';
--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -7,11 +7,7 @@ import { MapModule } from '../common/map/map.module';
 import { Daterangepicker } from 'ng2-daterangepicker';
 import { FormsModule } from '@angular/forms';
 
-import { RentalListComponent } from './rental-list/rental-list.component';
-import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
-import { RentalComponent } from './rental.component';
-import { RentalDetailComponent } from './rental-detail/rental-detail.component';
-import { RentalDetailBookingComponent } from './rental-detail/rental-detail-booking/rental-detail-booking.component';
+import { RentalComponent, RentalListItemComponent, RentalListComponent, RentalDetailComponent, RentalDetailBookingComponent } from './';
 
 import { RentalService } from './shared/rental.service';
 import { BookingService } from '../booking/shared/booking.service';
